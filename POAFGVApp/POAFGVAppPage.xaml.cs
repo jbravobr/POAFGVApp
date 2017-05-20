4467e8fac0d647c3921d8d913e8341b1bdc6ab59
Rodrigo Amaro <jbravo.br@gmail.com>
2017-05-20 14:29:54 -0300

    Commit inicial

---
+++ POAFGVApp/POAFGVAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace POAFGVApp
+{
+    public partial class POAFGVAppPage : ContentPage
+    {
+        public POAFGVAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
