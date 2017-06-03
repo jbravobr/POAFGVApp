c91ff769b1fb27675bff8f983c5ff8edee190d88
Rodrigo Amaro <jbravo.br@gmail.com>
2017-06-03 17:22:44 -0300

    Adicionando extensions para listas

--- POAFGVApp/Utils/Extensions/EnumDescriptionAttribute.cs
+++ POAFGVApp/Utils/Extensions/EnumDescriptionAttribute.cs
@@ -2,14 +2,14 @@
 namespace POAFGVApp
 {
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
-    public class EnumDescriptionAttribute : Attribute
-    {
-        readonly string _description;
-        public string Description { get { return _description; } }
+	    public class EnumDescriptionAttribute : Attribute
+	    {
+	        readonly string _description;
+	        public string Description { get { return _description; } }
 
-        public EnumDescriptionAttribute(string description)
-        {
-            _description = description;
-        }
-    }
+	        public EnumDescriptionAttribute(string description)
+	        {
+	            _description = description;
+	        }
+	    }
 }
