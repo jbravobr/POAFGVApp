594e87c42f9e8bae4e4ee624acd642989f4dbd3e
Rodrigo Amaro <jbravo.br@gmail.com>
2017-07-01 07:52:03 -0300

    Adicionado página de resumo de compra e confirmação de compra

---
+++ POAFGVApp/Views/CustomCells/PaymentDetailCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace POAFGVApp.Views
+{
+    public partial class PaymentDetailCell : ContentView
+    {
+        public PaymentDetailCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
